c235583b8621b9ee60898c363f18f6d2735287b5
sprajjwal <sprajjwal@ucdavis.edu>
2019-09-20 12:58:34 -0700

    [ADD]finished part 4, all pytest cases passed

--- superheroes.py
+++ superheroes.py
@@ -23,6 +23,8 @@ class Hero:
         self.name = name
         self.starting_health = starting_health
         self.current_health = starting_health
+        self.deaths = 0
+        self.kills = 0
 
     def add_ability(self, ability):
         self.abilities.append(ability)
@@ -40,6 +42,8 @@ class Hero:
         block = 0
         for armor in self.armors:
             block += armor.block()
+        if not self.is_alive():
+            return 0
         return block
 
     def take_damage(self, damage):
@@ -47,6 +51,12 @@ class Hero:
 
     def is_alive(self):
         return self.current_health > 0
+
+    def add_kill(self, num_kills):
+        self.kills += num_kills
+
+    def add_deaths(self, num_deaths):
+        self.deaths += num_deaths
 
     #helper function for fight
     @staticmethod
@@ -60,8 +70,12 @@ class Hero:
         self.fighting(self, opponent)
         if self.is_alive():
             print(f"{self.name} won!")
+            self.add_kill(1)
+            opponent.add_deaths(1)
         else:
             print(f"{opponent.name} won!")
+            self.add_deaths(1)
+            opponent.add_kill(1)
 
 class Weapon(Ability):
     def attack(self):
@@ -85,11 +99,59 @@ class Team:
     def add_hero(self, hero):
         self.heroes.append(hero)
 
+    @staticmethod
+    def is_team_alive(self):
+        ''' Helper function to check if everyone on team is alive '''
+        for hero in self.heroes:
+            if not hero.is_alive():
+                return False
+        return True
+
+    @staticmethod
+    def get_random_fighter(self):
+        if not self.is_team_alive(self): 
+            return 0
+        else: 
+            fighter = random.choice(self.heroes)
+            if not fighter.is_alive():
+                fighter = self.get_random_fighter(self)
+            return fighter
+
+    def attack(self, other_team):
+        dead = 0 # dead = 1 => self is dead | dead = 2 => other_team is dead
+        while dead == 0:
+            self_fighter = self.get_random_fighter(self)
+            other_team_fighter = other_team.get_random_fighter(other_team)
+            self_fighter.fight(other_team_fighter)
+            if self.is_team_alive(self):
+                dead = 1
+            elif other_team.is_team_alive(other_team):
+                dead = 2
+
+    def revive_heroes(self, health = 100):
+        for hero in self.heroes:
+            hero.current_health = health
+
+    def stats(self):
+        for hero in self.heroes:
+            ratio = hero.kills/hero.deaths
+            print(f"{hero.name}'s K/D ratio is: {ratio}'")
+
 if __name__ == "__main__":
-    team = Team("One")
+    team1 = Team("One")
     jodie = Hero("Jodie Foster")
-    team.add_hero(jodie)
-    assert team.heroes[0].name == "Jodie Foster"
-    team.remove_hero("Jodie Foster")
-    assert len(team.heroes) == 0
-    print(len(team.heroes))
+    ab =Ability("eat ass", 200)
+    jodie.add_ability(ab)
+    team1.add_hero(jodie)
+
+    team2 = Team("Two")
+    ww = Hero("Wonder Woman")
+    team2.add_hero(ww)
+
+    team1.heroes[0].fight(team2.heroes[0])
+    print(jodie.is_alive())
+    print(jodie.kills)
+
+    print(ww.is_alive())
+    print(ww.kills)
+    print(ww.deaths)
